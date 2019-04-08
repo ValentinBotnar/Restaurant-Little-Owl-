e7280d2dfbe3126025dddb675735246f3d139b05
Valentin <37238383+ValentinBotnar@users.noreply.github.com>
2019-04-08 11:17:27 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Restaurant.Models;
+using Restaurant.Models.Context;
+
+namespace Restaurant
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(connection));
+            services.AddDbContext<RegistrationContext>(options =>
+                options.UseSqlServer(connection));
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<RegistrationContext>();
+
+            services.AddMvc()
+                .AddSessionStateTempDataProvider();
+            services.AddSession();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseSession();
+            app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseCookiePolicy();
+            app.UseAuthentication();//////////////
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
